1429b550e11a55d017d0336e61cfad80b8164415
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-04-12 01:03:54 +0100

    Add tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,27 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+module.exports = {
+  content: [
+    "./js/**/*.js",
+    "../lib/*_web.ex",
+    "../lib/*_web/**/*.*ex"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#b62020",
+        secondary: "#ea3b2d",
+        tertiary: "#ef5e56",
+        quaternary: "#e49f9d",
+        quinary: "#ffffff",
+        success: "#008F05",
+        failure: "#FF4444",
+        warning: "#E09200",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+}; 
--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :parzival, ParzivalWeb.Endpoint,
   secret_key_base: "lYM3f4htWz3udVfEIAFIkTfh+Y8scBD+0sY9fOFoK9SI0/xwIO5fXCsNg7QBGdIU",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
