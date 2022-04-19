01108352510964c25053cd5953cf987ffa5dc1ce
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-04-19 01:27:49 +0100

    Fix alpine (#23)

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,9 +33,29 @@ let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute("content");
 
-let csrfToken = document
-  .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
+let Hooks = {};
+
+Hooks.CustomSelect = {
+  mounted() {
+    this.el.addEventListener("selected-change", (event) => {
+      this.pushEventTo(event.detail.id, "update", event.detail);
+    });
+
+    this.handleEvent("close-selected", (data) => {
+      const element = document.querySelector(data.id);
+
+      if (!element) return;
+      if (data.id !== `#${this.el.id}`) return;
+
+      element.dispatchEvent(new CustomEvent("reset"));
+
+      this.el.querySelector("input").value = data.value;
+      this.el
+        .querySelector("input")
+        .dispatchEvent(new Event("input", { bubbles: true }));
+    });
+  },
+};
 
 let liveSocket = new LiveSocket("/live", Socket, {
   params: { _csrf_token: csrfToken },
