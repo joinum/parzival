bc5189bb529a32f1b7075ae27450555a77654294
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-04-19 05:20:07 +0100

    Connect announcements to home

--- lib/parzival_web/live/home_live/index.ex
+++ lib/parzival_web/live/home_live/index.ex
@@ -1,6 +1,8 @@
 defmodule ParzivalWeb.HomeLive.Index do
   use ParzivalWeb, :live_view
 
+  alias Parzival.Tools
+  alias Parzival.Tools.Announcement
 
   @impl true
   def mount(_params, _session, socket) do
@@ -10,6 +12,7 @@ defmodule ParzivalWeb.HomeLive.Index do
   def handle_params(params, _url, socket) do
     {:noreply,
      socket
+     |> assign(:announcements, Enum.take(Tools.list_announcements(), 3))
      |> assign(:current_page, :home)}
   end
 
