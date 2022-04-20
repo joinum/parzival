09a9164de176ffa2f0074125741f6d39f7674f82
Luis Araújo <louis-1996@live.com.pt>
2022-04-20 07:53:03 +0100

    Add team page

--- lib/parzival_web/controllers/team_controller.ex
+++ lib/parzival_web/controllers/team_controller.ex
@@ -1,53 +1,57 @@
 defmodule ParzivalWeb.TeamController do
   use ParzivalWeb, :controller
 
+  @team [
+    %{
+      name: "Filipe Felício",
+      photo: "filipe",
+      organization: :cesium,
+      linkedin: "",
+      github: "",
+      twitter: ""
+    },
+    %{
+      name: "Filipe Felício",
+      photo: "filipe",
+      organization: :necc,
+      linkedin: "",
+      github: "",
+      twitter: ""
+    },
+    %{
+      name: "Filipe Felício",
+      photo: "filipe",
+      organization: :nefum,
+      linkedin: "",
+      github: "",
+      twitter: ""
+    },
+    %{
+      name: "Filipe Felício",
+      photo: "filipe",
+      organization: :cesium,
+      linkedin: "",
+      github: "",
+      twitter: ""
+    },
+    %{
+      name: "Filipe Felício",
+      photo: "filipe",
+      organization: :cesium,
+      linkedin: "",
+      github: "",
+      twitter: ""
+    }
+  ]
+
+  @organizers ["cesium","necc","nefum","di"]
+
   def index(conn, _params) do
-    team = [
-      %{
-        name: "Filipe Felício",
-        photo: "filipe",
-        organization: :cesium,
-        linkedin: "",
-        github: "",
-        twitter: ""
-      },
-      %{
-        name: "Filipe Felício",
-        photo: "filipe",
-        organization: :necc,
-        linkedin: "",
-        github: "",
-        twitter: ""
-      },
-      %{
-        name: "Filipe Felício",
-        photo: "filipe",
-        organization: :nefum,
-        linkedin: "",
-        github: "",
-        twitter: ""
-      },
-      %{
-        name: "Filipe Felício",
-        photo: "filipe",
-        organization: :cesium,
-        linkedin: "",
-        github: "",
-        twitter: ""
-      },
-      %{
-        name: "Filipe Felício",
-        photo: "filipe",
-        organization: :cesium,
-        linkedin: "",
-        github: "",
-        twitter: ""
-      }
-    ]
 
     conn
     |> assign(:current_page, "team")
-    |> assign(:team, team)
+    |> assign(:team, @team)
+    |> assign(:organizers, @organizers)
     |> render("index.html")
   end
 end
