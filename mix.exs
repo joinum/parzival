2d661840547ae77268d6bcfbf5a1b111f7085d4c
Luis Araújo <louis-1996@live.com.pt>
2022-04-30 18:07:21 +0100

    Fix docker environment

--- mix.exs
+++ mix.exs
@@ -41,24 +41,28 @@ defmodule Parzival.MixProject do
       # uploads
       {:waffle, "~> 1.1"},
       {:waffle_ecto, "~> 0.0"},
+
+      # development
+      {:dotenvy, "~> 0.6.0"},
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+
       {:bcrypt_elixir, "~> 2.0"},
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:timex, "~> 3.0"}
     ]
   end
