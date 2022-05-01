b636294b9892aa4b27947e57d8ce253bd226ce78
Rui Oliveira <rpoliveira02@gmail.com>
2022-05-01 12:16:00 +0100

    Fix styling

--- config/prod.exs
+++ config/prod.exs
@@ -19,11 +19,12 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-     config :parzival, ParzivalWeb.Endpoint,
-#       ...,
-       url: [scheme: "https", host: "join22-staging.herokuapp.com", port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
-       cache_static_manifest: "priv/static/cache_manifest.json"
+config :parzival, ParzivalWeb.Endpoint,
+  #       ...,
+  url: [scheme: "https", host: "join22-staging.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
 #       https: [
 #         ...,
 #         port: 443,
