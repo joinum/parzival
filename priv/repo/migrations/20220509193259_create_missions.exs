25d62a1c3e42c644538c75cd92b09e77cbd1b95a
ruioliveira02 <rpoliveira02@gmail.com>
2022-05-09 19:44:34 +0100

    Add migration

---
+++ priv/repo/migrations/20220509193259_create_missions.exs
@@ -0,0 +1,46 @@
+defmodule Parzival.Repo.Migrations.CreateMissions do
+  use Ecto.Migration
+
+  def change do
+    create table(:missions, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :title, :text
+      add :description, :text
+
+      timestamps()
+    end
+
+    create unique_index(:missions, [:title, :description])
+
+
+
+    create table(:tasks, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :title, :text
+      add :description, :text
+
+      add :mission_id, references(:missions, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+
+    create unique_index(:missions, [:title, :description, :mission_id])
+
+
+
+    create table(:task_completions, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :task_id, references(:tasks, on_delete: :nothing, type: :binary_id)
+      add :participant_id, references(:users, on_delete: :nothing, type: :binary_id)
+      add :staff_id, references(:users, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+
+    create unique_index(:missions, [:task_id, :participant_id])
+
+  end
+end
