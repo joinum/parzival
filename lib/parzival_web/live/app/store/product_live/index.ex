1863477a191f9bd228e7e4d92c26e498d4d4f64e
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-05-18 15:57:34 +0100

    Add store pagination (#101)

--- lib/parzival_web/live/app/store/product_live/index.ex
+++ lib/parzival_web/live/app/store/product_live/index.ex
@@ -2,13 +2,14 @@ defmodule ParzivalWeb.App.ProductLive.Index do
   @moduledoc false
   use ParzivalWeb, :live_view
 
+  import ParzivalWeb.Components.Pagination
+
   alias Parzival.Store
-  alias Parzival.Store.Product
   alias Parzival.Uploaders
 
   @impl true
-  def mount(params, _session, socket) do
-    {:ok, assign(socket, list_products(params))}
+  def mount(_params, _session, socket) do
+    {:ok, socket}
   end
 
   @impl true
@@ -16,25 +17,9 @@ defmodule ParzivalWeb.App.ProductLive.Index do
     {:noreply,
      socket
      |> assign(:current_page, :store)
-     |> apply_action(socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Product")
-    |> assign(:product, Store.get_product!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Product")
-    |> assign(:product, %Product{})
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Products")
-    |> assign(:product, nil)
+     |> assign(:page_title, "Store")
+     |> assign(:params, params)
+     |> assign(list_products(params))}
   end
 
   defp list_products(params) do
