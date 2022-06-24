95905185324f548b64fc3128eaa9422abcc77e8f
Nelson Estevão <nelsonmestevao@proton.me>
2022-06-24 13:20:04 +0100

    Add script to generate QR codes (#178)

---
+++ priv/scripts/generate_qrs.exs
@@ -0,0 +1,22 @@
+codes =
+  1..1000
+  |> Enum.map(fn n ->
+    index =
+      n
+      |> Integer.to_string
+      |> String.pad_leading(4, "0")
+
+    code = Ecto.UUID.generate()
+
+    qr =
+      "https://join.di.uminho.pt/profile/#{code}"
+      |> QRCodeEx.encode()
+      |> QRCodeEx.png(width: 460)
+
+    File.write("qrs/#{index}.png", qr, [:binary])
+
+    "#{index},#{code}"
+    |> IO.inspect
+  end)
+
+File.write("codes.csv", Enum.join(codes, "\n"))
