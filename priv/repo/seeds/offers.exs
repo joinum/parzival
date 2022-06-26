01ed841215e5c45560f4eb79873b015e0acd84b0
Nelson Estevão <nelsonmestevao@proton.me>
2022-06-26 19:23:31 +0100

    Fix job offers seeds (#211)

--- priv/repo/seeds/offers.exs
+++ priv/repo/seeds/offers.exs
@@ -4,18 +4,18 @@ defmodule Parzival.Repo.Seeds.Offers do
   alias Parzival.Repo
 
   alias Parzival.Accounts.User
-
+  alias Parzival.Companies.Application
   alias Parzival.Companies.Company
   alias Parzival.Companies.Offer
-  alias Parzival.Companies.OfferType
   alias Parzival.Companies.OfferTime
-
+  alias Parzival.Companies.OfferType
   alias Parzival.Tools.Post
 
   def run do
     seed_types()
     seed_times()
     seed_offers()
+    seed_applications()
   end
 
   def seed_types() do
@@ -136,6 +136,28 @@ defmodule Parzival.Repo.Seeds.Offers do
 
       _ ->
         Mix.shell().error("Found offers, aborting seeding offers.")
+    end
+  end
+
+  def seed_applications() do
+    users = Repo.all(where(User, role: :attendee))
+    offers = Repo.all(Offer)
+
+    case Repo.all(Application) do
+      [] ->
+        for _n <- 1..200 do
+          Application.changeset(
+            %Application{},
+            %{
+              user_id: Enum.random(users).id,
+              offer_id: Enum.random(offers).id
+            }
+          )
+          |> Repo.insert!()
+        end
+
+      _ ->
+        Mix.shell().error("Found applications, aborting seeding applications.")
     end
   end
 end
