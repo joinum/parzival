248571ad2794d1e4db5031c48415f40a3b352032
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-06-28 03:47:37 +0100

    Add connections (#226)

--- lib/parzival_web/live/app/profile_live/show.ex
+++ lib/parzival_web/live/app/profile_live/show.ex
@@ -69,10 +69,11 @@ defmodule ParzivalWeb.App.ProfileLive.Show do
      |> assign(:page_title, "Show User")
      |> assign(:params, params)
      |> assign(:user, user)
-     |> handle_role(user)}
+     |> handle_user_role(user)
+     |> handle_current_user_role(socket.assigns.current_user)}
   end
 
-  defp handle_role(socket, user) do
+  defp handle_user_role(socket, user) do
     case user.role do
       :recruiter ->
         socket
@@ -81,6 +82,33 @@ defmodule ParzivalWeb.App.ProfileLive.Show do
       :attendee ->
         socket
         |> assign(:curriculum, Gamification.get_user_curriculum(user))
+
+      _ ->
+        socket
+    end
+  end
+
+  defp handle_current_user_role(socket, user) do
+    case user.role do
+      :staff ->
+        socket
+        |> assign(
+          :connections,
+          Companies.list_connections(
+            where: [user_id: socket.assigns.user.id],
+            preloads: :company
+          )
+        )
+
+      :admin ->
+        socket
+        |> assign(
+          :connections,
+          Companies.list_connections(
+            where: [user_id: socket.assigns.user.id],
+            preloads: :company
+          )
+        )
 
       _ ->
         socket
