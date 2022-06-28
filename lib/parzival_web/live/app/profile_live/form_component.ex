1d7e88f6526dbdaedb1aa338949577f19edd2f85
Luis Araújo <l.araujoo.1996@gmail.com>
2022-06-28 16:57:24 +0100

    Connect recruiter to companies

--- lib/parzival_web/live/app/profile_live/form_component.ex
+++ lib/parzival_web/live/app/profile_live/form_component.ex
@@ -4,6 +4,7 @@ defmodule ParzivalWeb.App.ProfileLive.FormComponent do
 
   alias Parzival.Accounts
   alias Parzival.Accounts.User
+  alias Parzival.Companies
 
   @extensions_whitelist ~w(.jpg .jpeg .gif .png)
   @cycles [:Bachelors, :Masters, :Phd]
@@ -27,7 +28,8 @@ defmodule ParzivalWeb.App.ProfileLive.FormComponent do
      |> assign(assigns)
      |> assign(:user, user)
      |> assign(:cycles, @cycles)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> assign(list_companies(assigns))}
   end
 
   @impl true
@@ -96,6 +98,16 @@ defmodule ParzivalWeb.App.ProfileLive.FormComponent do
 
       _errors ->
         {:ok, user}
+    end
+  end
+
+  defp list_companies(params) do
+    case Companies.list_companies(params) do
+      {:ok, {companies, _meta}} ->
+        %{companies: companies}
+
+      {:error, _flop} ->
+        %{companies: []}
     end
   end
 end
