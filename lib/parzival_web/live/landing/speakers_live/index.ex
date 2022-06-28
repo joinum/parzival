08c35d4cae0ead8e26ca1ca1ca0df27a73b31fed
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-06-28 11:16:03 +0100

    Update speakers

--- lib/parzival_web/live/landing/speakers_live/index.ex
+++ lib/parzival_web/live/landing/speakers_live/index.ex
@@ -8,32 +8,54 @@ defmodule ParzivalWeb.Landing.SpeakersLive.Index do
     %{
       day: "28 de Junho",
       speakers: [
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"}
-      ]
-    },
-    %{
-      day: "29 de Junho",
-      speakers: [
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"}
-      ]
-    },
-    %{
-      day: "30 de Junho",
-      speakers: [
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"},
-        %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"}
+        %{
+          name: "Roberto Machado",
+          photo: "roberto",
+          job: "Co-Founder & CEO",
+          company: "Subvisual"
+        },
+        %{name: "Mário Gago", photo: "mario_gago", job: "CEO", company: "Pink Room"},
+        %{
+          name: "Diogo Viana",
+          photo: "diogo_viana",
+          job: "",
+          company: "Full Stack Software Engineer"
+        },
+        %{name: "André Maximino", photo: "join", job: "", company: "Pink Room"},
+        %{
+          name: "Miguel Palhas",
+          photo: "miguel_palhas",
+          job: "Blockchain Developer",
+          company: "Subvisual"
+        },
+        %{
+          name: "Diogo Teixeira",
+          photo: "diogo_teixeira",
+          job: "Advanced Application Specialist",
+          company: "Accenture"
+        }
       ]
     }
+    # %{
+    #   day: "29 de Junho",
+    #   speakers: [
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"}
+    #   ]
+    # },
+    # %{
+    #   day: "30 de Junho",
+    #   speakers: [
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Luis Araújo", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"},
+    #     %{name: "Filipe Felício", photo: "luis", job: "Organizer", company: "CeSIUM"}
+    #   ]
+    # }
   ]
 
   @impl true
