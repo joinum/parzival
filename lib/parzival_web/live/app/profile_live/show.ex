1f9743b0227a9fa68b022faff0731270804d3403
Rui Oliveira <rpoliveira02@gmail.com>
2023-06-05 17:13:37 +0100

    Fix connections

--- lib/parzival_web/live/app/profile_live/show.ex
+++ lib/parzival_web/live/app/profile_live/show.ex
@@ -16,8 +16,8 @@ defmodule ParzivalWeb.App.ProfileLive.Show do
   end
 
   @impl true
-  def handle_params(%{"qr" => qr}, _url, socket) do
-    user = Accounts.get_user_by_qr(qr)
+  def handle_params(%{"qr" => qr} = params, _url, socket) do
+    user = Accounts.get_user_by_qr(qr) |> Parzival.Repo.preload([:company])
 
     if Map.has_key?(socket.assigns, :current_user) and not is_nil(user) do
       if socket.assigns.current_user.role == :recruiter && user.role == :attendee do
@@ -27,10 +27,25 @@ defmodule ParzivalWeb.App.ProfileLive.Show do
           {:ok, _connection} ->
             {:noreply,
              socket
-             |> put_flash(:success, gettext("New Connection!"))}
+             |> put_flash(:success, gettext("New Connection!"))
+             |> assign(:current_page, :profile)
+             |> assign(:current_tab, user.role)
+             |> assign(:page_title, "Show User")
+             |> assign(:params, params)
+             |> assign(:user, user)
+             |> handle_user_role(user)
+             |> handle_current_user_role(socket.assigns.current_user)}
 
           {:error, _error} ->
-            {:noreply, socket}
+            {:noreply,
+             socket
+             |> assign(:current_page, :profile)
+             |> assign(:current_tab, user.role)
+             |> assign(:page_title, "Show User")
+             |> assign(:params, params)
+             |> assign(:user, user)
+             |> handle_user_role(user)
+             |> handle_current_user_role(socket.assigns.current_user)}
         end
       end
     else
@@ -57,7 +72,8 @@ defmodule ParzivalWeb.App.ProfileLive.Show do
         {:ok, _connection} ->
           {:noreply,
            socket
-           |> put_flash(:success, gettext("New Connection!"))}
+           |> put_flash(:success, gettext("New Connection!"))
+           |> assign(:current_page, :profile)}
 
         {:error, _error} ->
           {:noreply, socket}
